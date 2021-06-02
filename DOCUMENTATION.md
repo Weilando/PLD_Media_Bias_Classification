59a7472a3f0934c6549765ad22df82e0b19cb4f7
Weilando <Hendryk.Weiland@hhu.de>
2021-06-02 11:18:52 +0200

    refactor/docs: change feature set, minor improvements

--- DOCUMENTATION.md
+++ DOCUMENTATION.md
@@ -3,7 +3,7 @@
 ## Data Retrieval
 
 `data_retriever.py` can be used to retrieve tweets which mention PLDs from a given CSV-file.
-It offers a command line interface and saves its results into a CSV-file in the same directory as the input file.
+It offers a command line interface and saves aggregated features per PLD into a CSV-file in the same directory as the input file.
 
 ## Features
 
@@ -11,14 +11,17 @@ It offers a command line interface and saves its results into a CSV-file in the 
 
 Name | Value
 --- | ---
-tweet | nodeId of tweet
-plds | PLDs cited in the tweet
-tags | Hashtags used in the tweet, omits leading #
-emotion_pos | Float between 0 (weak) and 1 (strong)
-emotion_neg | Float between 0 (weak) and 1 (strong)
+pld | Name of PLD
+tags | Hashtags used in related tweets, omits leading #
+emotion_pos_avg | Average positive emotion score for related tweets
+emotion_neg_avg | Average negative emotion score for related tweets
+tweet_count | Number of aggregated related tweets
+tweet_ids | nodeIds of aggregated related tweets
+
+`tags` and `tweet_ids` represent used hashtags and nodeIds respectively.
+They are stored as strings using `+` as separator, which is not allowed in valid hashtags and nodeIds.
+The nodeIds might be used to hydrate the tweets.
 
 The knowledge base stores an emotion set containing negative and positive scores for each tweet.
-Please notice that for some tweets both values are greater than zero.
-
-`plds` and `tags` represent cited PLDs and used hashtags respectively.
-They are stored as strings using `+` as separator, which is not allowed in valid domain names and hashtags.
+`emotion_pos_avg` and `emotion_pos_avg` are floats between 0 (weak) and 1 (strong).
+Please notice that both values are greater than zero for some tweets.
--- src/main/python/data_retriever.py
+++ src/main/python/data_retriever.py
@@ -1,5 +1,5 @@
 """
-Retrieves tweets from the TweetsCOV19 dataset.
+Retrieves training data from the SPARQL-endpoint of TweetsCOV19.
 Example call: python -m data_retriever '../../../input_data/left_train.csv'
 """
 
@@ -19,36 +19,37 @@ def print_log(message, verbose):
         print(message)
 
 def is_valid_pld_path(rel_path):
-    """ Check if the given relative path exists and if the file ends with
-    '-_train.csv'. """
+    """ Checks if 'rel_path' exists and if its suffix is '-_train.csv'. """
     abs_path = os.path.join(os.getcwd(), rel_path)
     return os.path.exists(abs_path) and abs_path.endswith('_train.csv')
 
 def generate_tweets_path(rel_path):
-    """ Generate a relative path for a CSV-file with fetched tweets.
-    'rel_path' refers to a CSV-file with PLDs. """
+    """ Generates a relative path for a CSV-file with fetched tweets.
+    'rel_path' refers to a CSV-file with names of PLDs. """
     assert is_valid_pld_path(rel_path)
     return rel_path.replace('_train.csv', '_tweets.csv')
 
-def write_tweets_to_csv(rel_path, tweets, verbose=False):
-    """ Write 'tweets' into a CSV-file with the same relative path prefix as
-    the CSV-file which is referenced by 'rel_path'. """
+def write_tweets_to_csv(rel_path, tweets, header=False):
+    """ Writes 'tweets' into a CSV-file with the same relative path prefix as
+    the CSV-file which is referenced by 'rel_path'. If 'header' is True, a new
+    file is created with a header line. Otherwise, 'tweets' is appended. """
     assert is_valid_pld_path(rel_path)
 
     tweets_path = generate_tweets_path(rel_path)
-    print_log(f"Write tweets to '{tweets_path}'.", verbose)
-    with open(tweets_path, 'w', encoding='utf-8') as tweets_file:
-        fw = csv.writer(tweets_file, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+    with open(tweets_path, ('w' if header else 'a'), encoding='utf-8') as file:
+        fw = csv.writer(file, delimiter=',', quotechar='|',
+                        quoting=csv.QUOTE_MINIMAL)
 
-        fw.writerow(tweets["head"]["vars"])
+        if header:
+            fw.writerow(tweets["head"]["vars"])
         for tweet in tweets["results"]["bindings"]:
             fw.writerow([tweet[k]["value"] for k in tweet.keys()])
 
 # Data Retrieval
 
 def read_pld_list(rel_path, verbose=False):
-    """ Reads PLDs from a CSV-file referred to by rel_path.
-    Assumes that the CSV-file contains one PLD per line. """
+    """ Reads PLDs from a CSV-file referred to by 'rel_path'. Assumes that the
+    CSV-file contains the name of one PLD per line. """
     assert is_valid_pld_path(rel_path)
 
     pld_list, pld_counter = [], 0
@@ -64,12 +65,9 @@ def read_pld_list(rel_path, verbose=False):
     print_log(f"Read {pld_counter} PLDs from '{rel_path}'.", verbose)
     return pld_list
 
-def retrieve_tweets(rel_path, pld_list, verbose=False):
-    """ Retrieves training data from the SPARQL endpoint of TweetsCOV19.
-    All tweets related to the PLDs in pld_list are taken into account.
-    Returns the dataset as dict. """
-    print_log("Retrieve training data.", verbose)
-
+def retrieve_tweets(pld):
+    """ Retrieves aggregated training data from the TweetsCOV19 SPARQL-endpoint
+    and returnes a dict. All tweets related to 'pld' are taken into account. """
     sparql = SPARQLWrapper(SPARQL_URL)
     sparql.setReturnFormat(JSON)
     sparql.setQuery(f"""
@@ -80,48 +78,56 @@ def retrieve_tweets(rel_path, pld_list, verbose=False):
         PREFIX sioc_t: <http://rdfs.org/sioc/types#>
         PREFIX wna: <http://www.gsi.dit.upm.es/ontologies/wnaffect/ns#>
 
-        SELECT DISTINCT ?tweet,
-                        (GROUP_CONCAT(DISTINCT ?pld ; separator='+') AS ?plds),
-                        (GROUP_CONCAT(DISTINCT ?tag ; separator='+') AS ?tags),
-                        ?emotion_pos, ?emotion_neg
+        SELECT DISTINCT ?pld,
+            (GROUP_CONCAT(?tmp_tags ; separator='+') AS ?tags),
+            (AVG(?emotion_pos) AS ?emotion_pos_avg),
+            (AVG(?emotion_neg) AS ?emotion_neg_avg),
+            (COUNT(DISTINCT ?tweet) AS ?tweet_count),
+            (GROUP_CONCAT(DISTINCT ?tweet ; separator='+') AS ?tweet_ids)
         WHERE {{
-            ?tweet a sioc:Post ;
-                schema:citation ?url ;
-                schema:mentions ?mention ;
-                onyx:hasEmotionSet ?emotion_set .
-            BIND(REPLACE( REPLACE(STR(?url), "https?://(www.)?", ""),
-                          "/.*", "") as ?pld) .
-            FILTER(?pld IN (\"{'", "'.join(pld_list)}\")) .
+            SELECT ?tweet, ?pld,
+                (GROUP_CONCAT(DISTINCT ?tag ; separator='+') AS ?tmp_tags),
+                ?emotion_pos, ?emotion_neg
+            WHERE {{
+                ?tweet a sioc:Post ;
+                    schema:citation ?url ;
+                    schema:mentions ?mention ;
+                    onyx:hasEmotionSet ?emotion_set .
+                BIND(REPLACE( REPLACE(STR(?url), "https?://(www.)?", ""),
+                    "/.*", "") as ?pld) .
+                FILTER(?pld=\"{pld}\") .
 
-            ?mention a sioc_t:Tag ;
-                rdfs:label ?tag .
+                ?mention a sioc_t:Tag ;
+                    rdfs:label ?tag .
 
-            ?emotion_set a onyx:EmotionSet ;
-                onyx:hasEmotion ?emotion1 ;
-                onyx:hasEmotion ?emotion2 .
-            ?emotion1 onyx:hasEmotionCategory wna:negative-emotion ;
-                onyx:hasEmotionIntensity ?emotion_neg .
-            ?emotion2 onyx:hasEmotionCategory wna:positive-emotion ;
-                onyx:hasEmotionIntensity ?emotion_pos .
+                ?emotion_set a onyx:EmotionSet ;
+                    onyx:hasEmotion ?emotion1 ;
+                    onyx:hasEmotion ?emotion2 .
+                ?emotion1 onyx:hasEmotionCategory wna:negative-emotion ;
+                    onyx:hasEmotionIntensity ?emotion_neg .
+                ?emotion2 onyx:hasEmotionCategory wna:positive-emotion ;
+                    onyx:hasEmotionIntensity ?emotion_pos .
+            }}
+            GROUP BY ?pld ?tweet ?emotion_pos ?emotion_neg
         }}
-        ORDER BY ?tweet
+        GROUP BY ?pld
+        ORDER BY ?pld
     """)
-    # FILTER(?pld IN ("telegraaf.nl", "newbostonpost.com")).
-
     return sparql.queryAndConvert()
 
 # Main
 
 def parse_arguments(args):
     """ Creates an ArgumentParser with help messages. """
-    info =  """ Retriever for the TweetsCOV19 dataset.
-                Fetches tweets related to PLDs which are read from the CSV-file
-                which is referenced by 'rel_path' and writes them into a
-                JSON-file in the same directory. """
+    info =  """ Retriever for the TweetsCOV19 dataset. Fetches tweets related
+                to classified PLDs whose names are read from the CSV-file which
+                is referenced by 'rel_path'. It writes aggregated results per
+                PLD into a JSON-file in the same directory. """
     parser = ArgumentParser(description=info)
     parser.add_argument('-v', '--verbose', action='store_true', default=False,
                         help="activate output")
-    parser.add_argument('rel_path', help="relative path to training data, i.e., a CSV with PLDs")
+    parser.add_argument('rel_path',
+                        help="relative path to CSV-file with PLD names")
 
     if len(args) < 1:  # show help, if no arguments are given
         parser.print_help(sys.stderr)
@@ -132,9 +138,10 @@ def main(args):
     parsed_args = parse_arguments(args)
 
     pld_list = read_pld_list(parsed_args.rel_path, parsed_args.verbose)
-    tweets = retrieve_tweets(parsed_args.rel_path, pld_list,
-                             parsed_args.verbose)
-    write_tweets_to_csv(parsed_args.rel_path, tweets, parsed_args.verbose)
+    for n, pld in enumerate(pld_list, start=1):
+        print_log(f"{n:3d} Retrieve data for '{pld}'.", parsed_args.verbose)
+        tweets = retrieve_tweets(pld)
+        write_tweets_to_csv(parsed_args.rel_path, tweets, (n==1))
 
 if __name__ == '__main__':
     main(sys.argv[1:])
